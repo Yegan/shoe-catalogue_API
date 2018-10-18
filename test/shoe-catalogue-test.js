23bce4889da1bcdae2197fca3cdd0818e76db906
Yegan <yeganj@gmail.com>
2018-10-18 16:10:42 +0200

    created mocha tests

--- test/shoe-catalogue-test.js
+++ test/shoe-catalogue-test.js
@@ -1,3 +1,10 @@
-function ShoeCatalogue (){
+describe('The shoe catalogue add button should add shoes to display on the screen once a colour, size and brand are selected', function(){
+    it('The shoe catalogue should take in 3 paramaters of brand,size and colour and print an object', function(){
+        var ShoeCatalogue = ShoeCatalogues()
 
-};
+
+        assert.deepEqual('')
+
+    });
+
+});
---
+++ test/shoeFactory.js
@@ -0,0 +1,25 @@
+function ShoeCatalogues (){
+
+
+    // Create an object that stores all the shoe data
+    
+        // let shoes = {
+        //     id: '1'
+        //     brand: "nike",
+        //     size:"7",
+        //     color: "red",
+        //     stock: '2'
+        // }
+    
+        function addShoe(size,brand,colour){
+    
+        }
+    
+        return{
+            addShoe,
+            
+        }
+    
+    
+    
+    };
