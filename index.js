ee865ee2bb5599d3c62a1d290afaea31cc4f3ec6
Yegan <yeganj@gmail.com>
2018-10-22 16:11:32 +0200

    promises added to front-end

--- index.js
+++ index.js
@@ -1,52 +1,61 @@
 document.addEventListener('DOMContentLoaded', function () {
-const userTemplate = document.querySelector('.userTemplate').innerHTML
-const display = document.querySelector('.displayTable')
-const shoeBrand = document.querySelector('.brand')
-const compiled = Handlebars.compile(userTemplate)
+    const userTemplate = document.querySelector('.userTemplate').innerHTML
+    const display = document.querySelector('.displayTable')
+    const shoeBrand = document.querySelector('.brand')
+    const compiled = Handlebars.compile(userTemplate)
 
-// buttons
-const displayShoeBtn = document.querySelector('.filter')
-const filterBtn = document.querySelector('.custom-select')
+    // buttons
+    const displayShoeBtn = document.querySelector('.filter')
+    const filterBtn = document.querySelector('.custom-select')
 
-// instance of factory function
-const shoeFunc = ShoeCatalogues()
-const displayShoeFunc = shoeFunc.getShoes()
-
-// below object takes in a parameter
+    // instance of factory function
+    const shoeFunc = ShoeCatalogues()
 
 
-// display shoes button
-displayShoeBtn.addEventListener('click',function(){
-    display.innerHTML = compiled({shoes : displayShoeFunc})
+    function showShoes (shoes){
+        display.innerHTML = compiled({ shoes })
+    }
 
-})
+    function showAllShoes() {
+        shoeFunc
+            .getShoes()
+            .then(showShoes)
+            .catch(function(err) {
+                alert(err);
+            });
 
-//filter shoes button
-filterBtn.addEventListener('click', function(){
-    let brandOfShoe = shoeBrand.value   
-    const filterShoes = shoeFunc.filterShoes(brandOfShoe)
-    console.log(filterShoes)
-    display.innerHTML = compiled({shoes:filterShoes})
+        // below object takes in a parameter
+        // display shoes button
+        
+        
+    }
 
+    displayShoeBtn.addEventListener('click', showAllShoes);
 
-})
+    showAllShoes();
 
+    //filter shoes button
+    filterBtn.addEventListener('click', function () {
+        let brandOfShoe = shoeBrand.value
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if (brandOfShoe === '') {
+            return showAllShoes()
+        }
 
 
-    
+        shoeFunc
+            .filterShoes(brandOfShoe)
+            .then(showShoes)
+            .catch(function(err){
+                alert(err)
+            })
+        // showShoes(filterShoes);
+
+        // console.log(filterShoes)
+        // display.innerHTML = compiled({ shoes: filterShoes })
+
+
+    })
+
+
 });
--- test/shoeFactory.js
+++ test/shoeFactory.js
@@ -1,54 +1,58 @@
-function ShoeCatalogues (){
+function ShoeCatalogues() {
 
 
-    function getShoes(){
+    function getShoes() {
         const shoes = [
-          {
-              color: 'blue',
-              brand: 'Nike',
-              size: 5,
-              stock: 4
-          },
-          {
-              color: 'red',
-              brand: 'Addidas',
-              size: 6,
-              stock: 5
-          },
-          {
-              color:'black',
-              brand:'Puma',
-              size:7,
-              stock:3
-          }
+            {
+                color: 'blue',
+                brand: 'Nike',
+                size: 5,
+                stock: 4
+            },
+            {
+                color: 'red',
+                brand: 'Addidas',
+                size: 6,
+                stock: 5
+            },
+            {
+                color: 'black',
+                brand: 'Puma',
+                size: 7,
+                stock: 3
+            }
 
         ]
 
-        return shoes
+        return Promise.resolve(shoes)
 
     }
 
-    function filterShoes(filterCriteria){
-        let shoes = getShoes();
-        let list  = [];
-        
-        // filterCriteria.brand
-        // loop through a list of objects and find all the entries where the brands match
-        for (const shoe of shoes) {
-            if (shoe.brand === filterCriteria) {
-                list.push(shoe)
-            }
-        }
-        // put the matching entries in a list and return that list
+    function filterShoes(filterCriteria) {
+        return getShoes()
+                .then(function (shoes) {
+                    let list = [];
 
-        return list;
+                    // filterCriteria.brand
+                    // loop through a list of objects and find all the entries where the brands match
+                    for (const shoe of shoes) {
+                        if (shoe.brand === filterCriteria) {
+                            list.push(shoe)
+                        }
+                    }
+                    // put the matching entries in a list and return that list
+
+                    return list;
+                });
+
     }
 
-    
-    return{
+
+
+    return {
         getShoes,
         filterShoes,
     }
-    
-    
-    };
+
+
+};
