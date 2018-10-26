345de74f1db15b71826164a706d767543005d188
Yegan <yeganj@gmail.com>
2018-10-26 11:29:42 +0200

    setting up routes

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const flash = require('express-flash')
+const session = require('express-session')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const  = require('')
+const  = require('')
+const postgres = require('pg')
+const Pool = postgres.Pool
